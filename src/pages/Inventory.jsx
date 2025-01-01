72fd74adcca70de28dc0a2bf15f225cb673d9a2b
Piyush Goyal <piyush2022ug@gmail.com>
2025-01-02 04:32:12 +0530

    feat: RX2_HW3 completed

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,7 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchInventory } from "../actions.js";
+import { useEffect } from "react";
+
 export default function Inventory() {
-    return (
-        <>
-            <h1>Inventory</h1>
-        </>
-    )
+  const dispatch = useDispatch();
+  const inventory = useSelector((state) => state.inventory);
+
+  useEffect(() => {
+    dispatch(fetchInventory());
+  }, []);
+
+  return (
+    <>
+      <h1>Inventory</h1>
+      <h2>Total Inventory Items: {inventory.length}</h2>
+      <ul>
+        {inventory.map((items, index) => (
+          <li key={index}>
+            {items.itemName}: {items.itemQuantity}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/pages/RemovedItems.jsx
+++ src/pages/RemovedItems.jsx
@@ -1,7 +1,24 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchRemovedItems } from "../actions.js";
+import { useEffect } from "react";
+
 export default function RemovedItems() {
-    return (
-        <>
-            <h1>RemovedItems</h1>
-        </>
-    )
+  const dispatch = useDispatch();
+  const removedItems = useSelector((state) => state.removedItems);
+  useEffect(() => {
+    dispatch(fetchRemovedItems());
+  }, []);
+  return (
+    <>
+      <h1>Removed Items From Inventory</h1>{" "}
+      <h2>Removed Items Total: {removedItems.length}</h2>
+      <ul>
+        {removedItems.map((items, index) => (
+          <li key={index}>
+            {items.itemName}: {items.itemQuantity}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
